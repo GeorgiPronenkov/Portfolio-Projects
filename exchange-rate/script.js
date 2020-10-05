c8e7d898edc22448ad1a12465bde20ec68b21516
Georgi <georgi.pronenkov@gmail.com>
2020-10-06 00:31:59 +0300

    init

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -12,15 +12,15 @@ function calculate() {
   const currency_two = currencyEl_two.value;
 
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    .then(res => res.json())
-    .then(data => {
-      // console.log(data);
-      const rate = data.rates[currency_two];
+     .then(res => res.json())
+     .then(data => {
+       // console.log(data);
+       const rate = data.rates[currency_two];
 
-      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-    });
+       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+     });
 }
 
 // Event listeners
