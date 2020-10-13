396f192ff28469baced0d7a179583e40c3dd6bc4
Georgi <georgi.pronenkov@gmail.com>
2020-10-13 14:28:52 +0300

    temp

---
+++ website-react/src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Endless Route',
+    headLine: 'Follow your dream destination...'
+    description: 'Get access to our exclusive app that allows you to achieve your lifetime journey',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Mountain',
+    dark: true,
+    primary: true,
+    darkText: false
+};
--- website-react/src/components/InfoSection/InfoElements.js
+++ website-react/src/components/InfoSection/InfoElements.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
-export const = styled.div`
+export const InfoContainer = styled.div`
     color: #fff;
-    background: ${({lightBig}) => (lightBig ? '#f9f9f9' : '#010606')}; 
+    background: ${({ lightBig }) => (lightBig ? '#f9f9f9' : '#010606')}; 
    
     @media screen and (max-width: 768px){
        padding: 100px 0;
@@ -22,33 +22,77 @@ export const InfoWrapper = styled.div`
 `;
 
 export const InfoRow = styled.div`
+    display: grid;
+    grid-auto-columns: minmax(auto, 1fr);
+    align-items: center;
+    grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'`: `col1 col2`)};
     
+    @media screen and (max-width: 768px) {
+        // ${({imgStart}) => (imgStart ? `'col1' 'col2'`: `col1 col1' 'col2 col2'`)};
+    }
 `;
 
 export const Column1 = styled.div`
-    
+    margin-bottom: 15px;
+    padding: 0 15px;
+    grid-area: col1;
+`;
+
+export const Column2 = styled.div`
+    margin-bottom: 15px;
+    padding: 0 15px;
+    grid-area: col2;
 `;
 
 export const TextWrapper = styled.div`
-    
+    max-width: 540px;
+    padding-top: 0;
+    padding-bottom: 60px;
 `;
 
-export const TopLine = styled.div`
-    
+export const TopLine = styled.p`
+    color: #01bf71;
+    font-size: 16px;
+    line-height: 16px;
+    font-weight: 700;
+    letter-spacing: 1.4px;
+    text-transform: uppercase;
+    margin-bottom: 16px;
 `;
 
-export const Heading = styled.div`
+export const Heading = styled.h1`
+    margin-bottom: 24px;
+    font-size: 48px;
+    line-height: 1.1;
+    font-weight: 600;
+    color: black;
+    color: ${({ lightText}) => (lightText ? '#f7f8fa' : '#010606')};
     
+    @media screen and (max-width: 480px){
+        font-sie: 32px;
+    }
 `;
 
-export const Subtitle = styled.div`
-    
+export const Subtitle = styled.p`
+    max-width: 440px;
+    margin-bottom: 35px;
+    font-size: 18px;
+    line-height: 24px;
+    color: ${({darkText}) => (darkText ? '#010606' : '#fff')};
 `;
 
-export const BtnWrapp = styled.div`
-    
+export const BtnWrap = styled.div`
+    display: flex;
+    justify-content: flex-start;
 `;
 
-export const Button = styled.div`
-    
+export const ImgWrap = styled.div`
+    max-width: 555px;
+    height: 100%;
+`;
+
+export const Img = styled.div`
+    width: 100%;
+    margin: 0 0 10px 0; 
+    padding-right: 0;
 `;
