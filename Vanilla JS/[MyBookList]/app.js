984521344091f4d3deff20dd07bde611f1db7473
Georgi <georgi.pronenkov@gmail.com>
2020-11-23 22:20:14 +0200

    init

--- Vanilla JS/[MyBookList]/app.js
+++ Vanilla JS/[MyBookList]/app.js
@@ -25,7 +25,7 @@ class UI {
                         <td>${book.title}</td>    
                         <td>${book.author}</td>    
                         <td>${book.isbn}</td>    
-                        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>    
+                        <td><a href="#" class="btn btn-danger btn-sm delete">x</a></td>    
                         `;
 
         //6.append row to the list
